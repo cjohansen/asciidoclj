096d03b51b15f35669ef480d364adf9499e9e555
Christian Johansen <christian@cjohansen.no>
2014-01-14 19:01:00 +0100

    Upgrade midje

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.asciidoctor/asciidoctor-java-integration "0.1.4"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.0"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
