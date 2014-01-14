f64a2eb84cd6597287b61c95512c9d7ccd0d3c7c
Christian Johansen <christian@cjohansen.no>
2014-01-14 21:55:03 +0100

    Use asciidoctorj from source
    asciidoctor-java-integration 0.1.4 in maven central repository doesn't seem to
    include getDocumentStructure, which is the one method asciidoclj wants to wrap.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.asciidoctor/asciidoctor-java-integration "0.1.4"]]
+                 [org.asciidoctor/asciidoctorj "1.5.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]]
                    :plugins [[lein-midje "3.1.3"]]}})
