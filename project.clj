65e8e1033b5b15db7a12c521c05840cc4887ba47
Christian Johansen <christian@cjohansen.no>
2014-01-17 23:36:09 +0100

    Get asciidoctor 1.5.0-preview from bintray

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://github.com/cjohansen/asciidoclj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"bintray" "http://dl.bintray.com/lordofthejars/maven"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.asciidoctor/asciidoctorj "1.5.0-SNAPSHOT"]
+                 [org.asciidoctor/asciidoctor-java-integration "1.5.0.preview1"]
                  [org.slf4j/slf4j-simple "1.7.5"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]]
                    :plugins [[lein-midje "3.1.3"]]}})
